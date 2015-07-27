cc97faed15f7c1965e32cf791c9f6d8d54ea8cfc
dobrosi <dobrosi@gmail.com>
2015-07-27 21:35:53 +0200

    Update GpioService.java

--- src/main/java/com/programgyar/home/service/gpio/GpioService.java
+++ src/main/java/com/programgyar/home/service/gpio/GpioService.java
@@ -20,18 +20,6 @@ public class GpioService {
 		final GpioController gpio = GpioFactory.getInstance();
 
 		if (pinList == null) {
-			for (int pin = 0; pin < 16; pin++) {
-				int dir = GpioUtil.getDirection(pin);
-				int value = Gpio.digitalRead(pin);
-				PinPullResistance resistance = value == 0 ? PinPullResistance.PULL_DOWN : PinPullResistance.PULL_UP;
-				PinState state = value == 0 ? PinState.LOW : PinState.HIGH;
-				Pin gpioPin = RaspiPin.getPinByName("GPIO " + pin);
-				if (dir == GpioUtil.DIRECTION_IN) {
-					gpio.provisionDigitalInputPin(gpioPin, resistance);
-				} else {
-					gpio.provisionDigitalOutputPin(gpioPin, state);
-				}
-			}
 			pinList = gpio.getProvisionedPins().stream().map(f -> new PinDto(f)).collect(Collectors.toList());
 		}
 
@@ -43,9 +31,20 @@ public class GpioService {
 		gpio.getProvisionedPins().forEach(f -> f.addListener(listener));
 	}
 
-	public static void startGpio(Pin pin) {
+	public static void startGpio() {
 		final GpioController gpio = GpioFactory.getInstance();
 
-		gpio.provisionDigitalInputPin(pin, PinPullResistance.PULL_DOWN);
+		for (int pin = 0; pin < 16; pin++) {
+			int dir = GpioUtil.getDirection(pin);
+			int value = Gpio.digitalRead(pin);
+			PinPullResistance resistance = value == 0 ? PinPullResistance.PULL_DOWN : PinPullResistance.PULL_UP;
+			PinState state = value == 0 ? PinState.LOW : PinState.HIGH;
+			Pin gpioPin = RaspiPin.getPinByName("GPIO " + pin);
+			if (dir == GpioUtil.DIRECTION_IN) {
+				gpio.provisionDigitalInputPin(gpioPin, resistance);
+			} else {
+				gpio.provisionDigitalOutputPin(gpioPin, state);
+			}
+		}
 	}
 }
